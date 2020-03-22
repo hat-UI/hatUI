137ac68bbee2a5a2a0eb94f103fd80cf027c013c
Harhao <huahaoworkspace@163.com>
2020-03-22 11:08:48 +0800

    add the code coverage

--- build/webpack.test.conf.js
+++ build/webpack.test.conf.js
@@ -3,18 +3,53 @@ const merge = require("webpack-merge");
 const config = require("../config");
 const baseCongfig = require("./webpack.base.conf")
 const nodeExternals = require('webpack-node-externals')
-module.exports = merge(baseCongfig,{
-    mode: 'production',
-    devtool: 'inline-cheap-module-source-map',
-    entry: path.resolve(__dirname, "../src/index.js"),
-    output: {
-      filename:`${config.packageName}.min.js`,
-      path: path.resolve(__dirname, "../dist"),
-      libraryTarget: 'umd',
-      globalObject: 'this',
-      umdNamedDefine: true,
-      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-      devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+const isCoverage = process.env.NODE_ENV === 'coverage';
+const webpackConfig = merge(baseCongfig, {
+  mode: 'production',
+  devtool: 'inline-cheap-module-source-map',
+  entry: path.resolve(__dirname, "../src/index.js"),
+  output: {
+    filename: `${config.packageName}.min.js`,
+    path: path.resolve(__dirname, "../dist"),
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+      ]
     },
-    externals: [nodeExternals()]
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            prependData: `@import "./src/styles/index.scss"; `
+          }
+        }
+      ]
+    }]
+  }
 });
+
+isCoverage && webpackConfig.module.rules.unshift({
+  test: /\.(js|ts)/,
+  use: {
+    loader: 'istanbul-instrumenter-loader',
+    options: { esModules: true }
+  },
+  include: path.resolve(__dirname, '../src/packages/')
+})
+
+module.exports = webpackConfig
