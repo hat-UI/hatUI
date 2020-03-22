d29066a7c96ecf3c4c31ce0039436e36946ca29c
harhao <huahaoworkspace@163.com>
2020-03-22 14:04:57 +0800

    fix: remove the eslint warning

--- src/packages/button/index.js
+++ src/packages/button/index.js
@@ -2,7 +2,7 @@ import Button from './button.vue';
 import './button.scss';
 
 
-Button.install = function (Vue) {
+Button.install = function install(Vue) {
   Vue.component(Button.name, Button);
 };
 
