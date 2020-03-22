7e997e1b0317054ce61ba877afc8e9b64e67fe0d
harhao <huahaoworkspace@163.com>
2020-03-22 22:07:53 +0800

    add the document

--- sites/doc/.vuepress/config.js
+++ sites/doc/.vuepress/config.js
@@ -10,88 +10,94 @@ module.exports = {
         nav: [
             {
                 text: '指南',
-                link: '/pages/guide/index.md'
+                link: '/pages/component/introduce.md'
             }, {
                 text: '组件',
-                link: '/pages/component/index.md'
+                link: '/pages/component/cell.md'
 
             }, {
                 text: '主题',
-                link: '/pages/theme/index.md'
+                link: '/pages/component/theme.md'
             }, {
                 text: '示例',
-                link: '/pages/demo/index.md'
+                link: '/pages/component/demo.md'
             }, {
                 text: 'Github',
                 link: 'https://github.com/hat-UI/hatUI'
             }
         ],
         sidebar: {
-            '/pages/component/':[         
+            '/pages/component/': [
                 {
-                    title: '介绍',
+                    title: '指南',
                     collapsable: false,
-                    sidebarDepth: 1,
-                    children: []
-                }, {
-                    title: '快速上手',
-                    collapsable: false,
-                    sidebarDepth: 1
-                }, {
-                    title: '主题定制',
-                    collapsable: false,
-                    sidebarDepth: 1
-                }, {
-                    title: '国际化',
-                    collapsable: false,
-                    sidebarDepth: 1
-                }, {
-                    title: '更新日志',
-                    collapsable: false,
-                    sidebarDepth: 1
-                }, 
-                {
-                    title: '布局组件',
-                    collapsable: false,
-                    sidebarDepth: 1,
-                    children: []
-                }, {
-                    title: '操作反馈',
-                    collapsable: false,
-                    sidebarDepth: 1,
-                    children: []
-                }, {
-                    title: '基础组件',
-                    collapsable: true,
-                    sidebarDepth: 1,
+                    sidebarDepth: 0,
                     children: [
-                        ['icon.md', 'Icon 图标'],
-                        ['button.md','Button 按钮'],
-                        ['uploader.md','ButtonGroup 按钮组'],
-                        ['buttonGroup.md','uploader 上传'],
-                        ['avatar.md','Avatar 头像'],
-                        ['popup.md','popUp 弹出层']
+                        ['introduce.md', '快速上手'],
+                        ['theme.md', '主题定制'],
+                        ['internation.md', '国际化'],
+                        ['update.md', '更新日志']
                     ]
-                }, {
-                    title: '导航组件',
+                },
+                {
+                    title: '组件',
                     collapsable: false,
-                    sidebarDepth: 1,
-                    children: []
-                }, {
-                    title: '数据录入',
-                    collapsable: false,
-                    sidebarDepth: 1,
-                    children: []
-                }, {
-                    title: '数据展示',
-                    collapsable: false,
-                    sidebarDepth: 1,
-                    children: []
-                }, {
-                    title: '业务组件',
-                    collapsable: false,
-                    sidebarDepth: 1,
-                    children: []
+                    sidebarDepth: 2,
+                    children: [
+                        {
+                            title: '布局组件',
+                            collapsable: true,
+                            sidebarDepth: 0,
+                            children: [
+                                ["cell.md","Cell 列表项"],
+                                ["flex.md","Flex flex布局"],
+                            ]
+                        }, {
+                            title: '操作反馈',
+                            collapsable: false,
+                            sidebarDepth: 0,
+                            children: []
+                        }, {
+                            title: '基础组件',
+                            collapsable: true,
+                            sidebarDepth: 0,
+                            children: [
+                                ['icon.md', 'Icon 图标'],
+                                ['button.md', 'Button 按钮'],
+                                ['uploader.md', 'ButtonGroup 按钮组'],
+                                ['buttonGroup.md', 'uploader 上传'],
+                                ['avatar.md', 'Avatar 头像'],
+                                ['popup.md', 'popUp 弹出层']
+                            ]
+                        }, {
+                            title: '导航组件',
+                            collapsable: true,
+                            sidebarDepth: 0,
+                            children: [
+                                ['tab.md', 'Tab 选项卡'],
+                                ['tabBar.md', 'TabBar 标签栏'],
+                                ['navBar.md', 'NavBar 导航栏'],
+                                ['noticeBar.md', 'NoeticeBar 公告栏'],
+                                ['menu.md', 'Menu 菜单'],
+                                ['elevator.md', ' Elevator 电梯楼层']
+                            ]
+                        }, {
+                            title: '数据录入',
+                            collapsable: false,
+                            sidebarDepth: 0,
+                            children: []
+                        }, {
+                            title: '数据展示',
+                            collapsable: false,
+                            sidebarDepth: 0,
+                            children: []
+                        }, {
+                            title: '业务组件',
+                            collapsable: false,
+                            sidebarDepth: 0,
+                            children: []
+                        }
+                    ]
                 }
             ]
         }
--- sites/doc/README.md
+++ sites/doc/README.md
@@ -2,9 +2,9 @@
 home: true,
 heroImage: /hat.png
 heroText: hat-UI 文档
-tagline: 移动端网站快速成型工具
+tagline: 移动端网站快速成型工具component
 actionText: 立刻了解
-actionLink: /pages/guide/index.md
+actionLink: /pages/component/introduce.md
 features:
 - title: 简洁至上
   details: 以最简洁的案例帮助你快速了解hatUI的使用方法
