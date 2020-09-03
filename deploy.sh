aae9d5eb7b64a310e884540a9082ee7bb819844c
huahao <lvhuahao@ad61v1.com>
2020-09-03 11:39:56 +0800

    feat: sh添加vuepress

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git commit -m 'update article'
 
 # 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
 # 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
-git push -f git@github.com:hat-UI/hat-UI.githib.io.git master
+git push -f "https://${GITHUB_ToKEN}@github.com/hat-UI/hatUI.git" function_comp_dev
 
 # 打包生成静态文件
 npm run prod:demo
