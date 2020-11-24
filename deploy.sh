d35396ed19be016df1579bcf8f112d0213eb217d
harhao <huahaoworkspace@163.com>
2020-11-24 22:21:17 +0800

    fix: 修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,21 +14,21 @@ git commit -m 'update article'
 
 # 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
 # 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
-git push -f git@github.com:hat-UI/hat-UI.githib.io.git function_comp_dev
+git push -f git@github.com:hat-UI/hat-UI.githib.io.git master
 
 # 打包生成静态文件
 npm run prod:demo
 
-# 进入打包好的文件夹
-cd ../../../demo
+# # 进入打包好的文件夹
+# cd ../../../demo
 
-# 创建git的本地仓库，提交修改
-git init
-git add -A
-git commit -m 'update demo'
+# # 创建git的本地仓库，提交修改
+# git init
+# git add -A
+# git commit -m 'update demo'
 
-# 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
-# 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
-git push -f https://gitee.com/Harhao/hat-ui.git master
+# # 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
+# # 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
+# git push -f https://gitee.com/Harhao/hatui-site.git master
 
 cd -
