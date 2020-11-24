0334ba6d07f730a8ee36b030c0633956ed0d527e
harhao <huahaoworkspace@163.com>
2020-11-24 22:55:09 +0800

    fix: 修改deploy

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ git commit -m 'update article'
 
 # 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
 # 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
-git push -f git@github.com:hat-UI/hat-UI.githib.io.git master
-
+# git push -f git@github.com:hat-UI/hat-UI.githib.io.git master
+git push -f git@gitee.com:Harhao/hat-UI.github.io.git master
 # 打包生成静态文件
 npm run prod:demo
 
